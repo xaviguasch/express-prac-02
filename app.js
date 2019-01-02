c01f27b231d7b1f9475e0ca618b20d013857c130
xaviguasch <xaviguasch@gmail.com>
2019-01-02 13:11:33 +0100

    adding partials to the templates

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.render('home.ejs')
+    res.render('home')
 })
 
 app.get('/fallinlovewith/:thing', function(req, res) {
     const thing = req.params.thing
-    res.render('love.ejs', {thingVar: thing})
+    res.render('love', {thingVar: thing})
 })
 
 app.get('/posts', function(req, res) {
@@ -17,7 +21,7 @@ app.get('/posts', function(req, res) {
         { title: "Can you believe this?", author: "Claire"},
     ]
 
-    res.render('posts.ejs', {posts: posts})
+    res.render('posts', {posts: posts})
 })
 
 app.listen(3000, 'localhost', function() {
