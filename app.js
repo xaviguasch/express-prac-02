9b998e09fa4540a2e83886a4d6f5e0ef0129bc29
xaviguasch <xaviguasch@gmail.com>
2019-01-02 11:48:34 +0100

    adding ejs pt2

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
     res.render('home.ejs')
 })
 
+app.get('/fallinlovewith/:thing', function(req, res) {
+    const thing = req.params.thing
+    res.render('love.ejs', {thingVar: thing})
+})
 
 app.listen(3000, 'localhost', function() {
     console.log('server has started, listening on port 3000, frendo!!!');
