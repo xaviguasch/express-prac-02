87794ec2a5a19ad883ef2dad692bf12fe08f7190
xaviguasch <xaviguasch@gmail.com>
2019-01-02 12:40:21 +0100

    adding logic to ejs

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/fallinlovewith/:thing', function(req, res) {
     res.render('love.ejs', {thingVar: thing})
 })
 
+app.get('/posts', function(req, res) {
+    const posts = [
+        { title: "Post 1", author: "Suzy"},
+        { title: "My adorable pet Bunny", author: "Martha"},
+        { title: "Can you believe this?", author: "Claire"},
+    ]
+
+    res.render('posts.ejs', {posts: posts})
+})
+
 app.listen(3000, 'localhost', function() {
     console.log('server has started, listening on port 3000, frendo!!!');
 })
